fced4d8fb81d7f48561a15678971ee5ce44ce72d
Nguyen Quoc Hop <hop35889@gmail.com>
2023-06-02 13:59:43 +0700

    RadialBarChart

--- src/containers/Charts/MyPieChart.jsx
+++ src/containers/Charts/MyPieChart.jsx
@@ -26,20 +26,14 @@ const data = [
 ];
 
 const style = {
-    top: '100%',
-    left: '50%',
-    transform: 'translate(-50%, -100%)',
-    lineHeight: '24px',
 
 };
-const CustomizedLegend = (value, entry) => {
+const CustomizedLegend = (value, entry, index) => {
     return (
-        <>
-            <div>
-                <span style={{ color: '#5f5F76', marginLeft: '10px', marginRight: '30px' }}>{entry.payload.name}</span>
-                <span style={{ color: '#0F0F3F', fontWeight: 700 }}>${entry.payload.uv.toFixed(2)}</span>
-            </div>
-        </>
+        <div style={{with:'50%'}}>
+            <span style={{ color: '#5f5F76', marginLeft: '10px', marginRight: '30px' }}>{entry.payload.name}</span>
+            <span style={{ color: '#0F0F3F', fontWeight: 700 }}>${entry.payload.uv}</span>
+        </div>
     );
 };
 
@@ -47,13 +41,7 @@ export const MyPieChart = () => {
 
     return (
         <>
-            <p
-                style={{
-                    color: "#4A4A65",
-                    fontWeight: 700,
-                    fontSize: "32px",
-                }}
-            >
+            <p style={{ color: "#4A4A65", fontWeight: 700, fontSize: "32px" }}>
                 Money Allocation
             </p>
             <RadialBarChart startAngle={90} endAngle={450} width={537} height={541} cx="50%" cy="50%" innerRadius="10%" outerRadius="80%" barSize={10} data={data}>
@@ -64,7 +52,7 @@ export const MyPieChart = () => {
                     dataKey="uv"
                 />
                 {/* <Legend iconType='circle' width={"50%"} iconSize={24} layout="horizontal" verticalAlign='middle' wrapperStyle={style} formatter={CustomizedLegend} /> */}
-                <Legend layout="horizontal" verticalAlign="bottom" align="center" />
+                <Legend iconType='circle' iconSize={24} layout="horizontal" verticalAlign="bottom" align="center"  wrapperStyle={style} formatter={CustomizedLegend} />
             </RadialBarChart>
         </>
     );
